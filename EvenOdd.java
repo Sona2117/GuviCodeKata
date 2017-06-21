ada1928325461158e6430a90bee237e2ae1e6465
Sona2117 <sonathanu1997@gmail.com>
2017-06-21 09:24:40 +0530

    Create EvenOdd.java

---
+++ EvenOdd.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class EvenOdd{
+	public static void main(String[] args){
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the Number");
+		int num=s.nextInt();
+		if(num%2==0)
+		{
+			System.out.println("The Given Number is Even");			
+		}
+		else
+		{
+			System.out.println("The Given Number is Odd");			
+		}
+	}
+}
+		
