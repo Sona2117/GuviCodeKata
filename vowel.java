44560988777efdf07140c4609b918e723ac06b53
Sona2117 <sonathanu1997@gmail.com>
2017-06-22 09:14:01 +0530

    Create vowel.java

---
+++ vowel.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+public class three{
+	public static void main(String[] args){
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the character to be checked");
+		char ch=s.next().charAt(0);
+		switch(ch)
+		{
+			case 'a':
+				System.out.println("a is vowel");
+				break;
+			case 'e':
+				System.out.println("e is vowel");
+				break;
+			case 'i':
+				System.out.println("i is vowel");
+				break;
+			case 'o':
+				System.out.println("o is vowel");
+				break;
+			case 'u':
+				System.out.println("u is vowel");
+				break;
+			case 'A':
+				System.out.println("A is vowel");
+				break;				
+			case 'E':
+				System.out.println("E is vowel");
+				break;
+			case 'I':
+				System.out.println("I is vowel");
+					break;
+			case 'O':
+				System.out.println("O is vowel");
+				break;
+			case 'U':
+				System.out.println("U is vowel");
+				break;
+			default:
+				System.out.println("consonant");
+				
+		}	
+		
+	}
+}
